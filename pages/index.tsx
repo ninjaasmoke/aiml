d2681e1b5e12ba76ebd240b6914001267266dc8c
ninjaasmoke <nithins674@gmail.com>
2022-02-05 00:28:21 +0530

    animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import Link from 'next/link'
 import { progs } from '../data/progs'
 import styles from '../styles/Home.module.css'
+import Typed from "react-typed";
 
 const Home: NextPage = () => {
   return (
@@ -13,6 +14,13 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1>AI ML Programs</h1>
+        <Typed
+            strings={["VTU 2018 Scheme", "7th Semester"]}
+            typeSpeed={40}
+            backSpeed={50}
+            backDelay={3000}
+            // loop
+          />
         <hr />
         <div className={styles.grid}>
           {
