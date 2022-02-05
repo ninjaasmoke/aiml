580b6d77d7bcd72224f72e519b94d5837038aa23
ninjaasmoke <nithins674@gmail.com>
2022-02-05 23:36:15 +0530

    code correction

--- data/progs.ts
+++ data/progs.ts
@@ -102,8 +102,8 @@ y_pred=classifier.predict(x_test)
 print(len(x_test))
 
 for r in range(0,len(x_test)):
-print(" Sample:", str(x_test[r]), " Actual-label:", str(y_test[r]), " Predictedlabel:",str
-print("Classification Accuracy :" , classifier.score(x_test,y_test));
+	print(" Sample:", str(x_test[r]), " Actual-label:", str(y_test[r]), " Predictedlabel:",str
+	print("Classification Accuracy :" , classifier.score(x_test,y_test));
 
 x_test = [[3,4,5,2],[5,4,5,2]]
 y_pred=classifier.predict(x_test)
