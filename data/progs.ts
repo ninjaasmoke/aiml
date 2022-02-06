5868cb34a5174874278898b1be995b138e91634c
ninjaasmoke <nithins674@gmail.com>
2022-02-06 11:55:05 +0530

    code

--- data/progs.ts
+++ data/progs.ts
@@ -85,7 +85,7 @@ from sklearn import datasets
 iris=datasets.load_iris()
 print("Iris Data set loaded...")
 print(iris.data.shape)
-print(iris.feature_name
+print(iris.feature_name)
 print(iris.target_names)
 print(iris.data)
 print(iris.target)
